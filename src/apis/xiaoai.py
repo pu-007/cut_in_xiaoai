9707cff76b54da367ea4639fb29df83d7e4f9750
pu-007 <pu.007@qq.com>
2024-06-02 17:18:46 +0800

    chore(xiaoai.py): add offcial document link

--- src/apis/xiaoai.py
+++ src/apis/xiaoai.py
@@ -1,1 +1,2 @@
-#TODO add Xiaoai API support, getting the input circularly like `xiaoai_gpt`
+# https://developers.xiaoai.mi.com/documents/Home?type=/api/doc/render_markdown/SkillAccess/skill/CustomSkillsMain
+# TODO add Xiaoai API support, getting the input circularly like `xiaoai_gpt`
